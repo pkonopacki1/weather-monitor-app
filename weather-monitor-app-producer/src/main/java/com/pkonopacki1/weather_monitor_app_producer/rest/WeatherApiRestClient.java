4805cbdc203f7844cf94b8d6e710da10f58d808c
piotr.konopacki <konopackipio@gmail.com>
2025-05-28 08:14:05 +0200

    Adjustment in rest return value

--- weather-monitor-app-producer/src/main/java/com/pkonopacki1/weather_monitor_app_producer/rest/WeatherApiRestClient.java
+++ weather-monitor-app-producer/src/main/java/com/pkonopacki1/weather_monitor_app_producer/rest/WeatherApiRestClient.java
@@ -3,21 +3,19 @@ package com.pkonopacki1.weather_monitor_app_producer.rest;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 import com.pkonopacki1.weather_monitor_app_producer.rest.model.WeatherResponse;
 
 @Component
-@EnableScheduling
+// @EnableScheduling
 public class WeatherApiRestClient {
 
         @Value("${weather.api.key}")
         private String apiKey;
 
-        private static final String baseUrl = "https://api.weatherapi.com/v";
+        private static final String baseUrl = "https://api.weatherapi.com/v1";
 
         public Optional<WeatherResponse> getCityWeather(String city) {
                 var restClient = RestClient.builder()
@@ -31,14 +29,14 @@ public class WeatherApiRestClient {
                                                 .queryParam("q", city)
                                                 .build())
                                 .retrieve()
-                                .body(String.class);
+                                .body(WeatherResponse.class);
 
-                return Optional.empty();
+                return responseBody != null ? Optional.of(responseBody) : Optional.empty();
         }
 
-        @Scheduled(fixedRate = 1000)
-        public void test() {
-                getCityWeather("POZNAN");
-        }
+        // @Scheduled(fixedRate = 10000)
+        // public void test() {
+        // getCityWeather("POZNAN");
+        // }
 
 }
